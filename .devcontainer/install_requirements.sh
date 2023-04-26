399532653802bdec75bcfc9219bb049ffe4538ea
Sang-Yun Oh <syoh@users.noreply.github.com>
2023-04-26 21:13:32 +0000

    update

--- .devcontainer/install_requirements.sh
+++ .devcontainer/install_requirements.sh
@@ -2,8 +2,16 @@
 
 mkdir -p ${HOME}/.local/bin
 
-if [[ ! -e ~/.local/yadm-project ]]
-then
+REQD=()
+type -p zsh >/dev/null || REQD+=(zsh)
+type -p git >/dev/null || REQD+=(git)
+
+if [ -z "$REQD"]; then
+    sudo apt update
+    sudo apt install -y $REQD
+fi
+
+if [[ ! -e ~/.local/yadm-project ]]; then
     git clone https://github.com/TheLocehiliosan/yadm.git ~/.local/yadm-project && \
         ln -s ~/.local/yadm-project/yadm ~/.local/bin/yadm
 fi
