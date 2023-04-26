25e8732bea1d8c2ffc1473565da6c16b0735b2fc
Sang-Yun Oh <syoh@users.noreply.github.com>
2023-04-26 21:14:13 +0000

    spacing

--- .devcontainer/install_requirements.sh
+++ .devcontainer/install_requirements.sh
@@ -6,7 +6,7 @@ REQD=()
 type -p zsh >/dev/null || REQD+=(zsh)
 type -p git >/dev/null || REQD+=(git)
 
-if [ -z "$REQD"]; then
+if [ -z "$REQD" ]; then
     sudo apt update
     sudo apt install -y $REQD
 fi
