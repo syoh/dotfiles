40e7f605c0b9f2cf4d2d9f605464c39a89f5208e
Sang-Yun Oh (Sang) <syoh@ucsb.edu>
2024-12-28 23:40:35 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # try this later: https://superuser.com/questions/39751/add-directory-to-path-if-its-not-already-there
-LOCAL_BIN=$HOME/.local/bin
-if [ ! -d $LOCAL_BIN ]
+export LOCAL_BIN="${HOME}/.local/bin"
+if [ ! -d "${LOCAL_BIN}" ]
 then
-    mkdir -p $LOCAL_BIN
+    mkdir -p "${LOCAL_BIN}"
 fi
-export PATH=$LOCAL_BIN:$PATH
+export PATH="${LOCAL_BIN}:$PATH"
 
 YADM_VERSION=3.3.0
 wget https://github.com/TheLocehiliosan/yadm/archive/refs/tags/${YADM_VERSION}.tar.gz -O - \
