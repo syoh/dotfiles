4bdfb97181af5a8f92aa53fb105388cd777152e2
Sang-Yun Oh <syoh@users.noreply.github.com>
2025-01-08 23:03:07 +0000

    install ohmyzsh if zsh is installed

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if zsh is installed
+if ! command -v zsh &> /dev/null; then
+  echo "zsh is not installed. Please install zsh first."
+  exit 0
+fi
+
 # Check if Oh My Zsh is installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Oh My Zsh not found. Installing..."
